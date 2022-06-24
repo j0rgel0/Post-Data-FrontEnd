936dffb3078bdcb0735e2de11ee179e001cd60f8
Eric Rodrigo Diaz Romero <eric.diaz.r@hotmail.com>
2022-06-24 12:54:20 -0500

    Creación de archivos JSON de registro cuenta y productos

--- js/registroProductos.js
+++ js/registroProductos.js
@@ -18,30 +18,30 @@ botonEnviar.addEventListener("click", (event)=> {
      validaciones.descripcion=validarDescripcion(txtDescripcion.value);
     validaciones.precio=validarPrecio(txtPrecio.value);
      if (Object.values(validaciones).every((value) => value === true) ){
-         console.log("Hola");
-     }
-    let elemento =  `
-    {"nombre" : "${txtNombre.value}",
-    "autor" : "${txtAutor.value}",
-    "img" : "${txtURL.value}",
-    "editorial" : "${txtEditorial.value}",
-    "descripcion" : "${txtDescripcion.value}",
-    "isbn" : "${txtISBN.value}",
-    "precio" : "${txtPrecio.value}"}`;
+        let elemento =  `
+        {"nombre" : "${txtNombre.value}",
+        "autor" : "${txtAutor.value}",
+        "img" : "${txtURL.value}",
+        "editorial" : "${txtEditorial.value}",
+        "descripcion" : "${txtDescripcion.value}",
+        "isbn" : "${txtISBN.value}",
+        "precio" : "${txtPrecio.value}"}`;
+        
+        //JSON a JS
+        datos = JSON.parse(localStorage.getItem("productos"));
+        datos.push(JSON.parse(elemento));
     
-    //JSON a JS
-    datos = JSON.parse(localStorage.getItem("productos"));
-    datos.push(JSON.parse(elemento));
-
-    // Crear de nuevo JSON
-    localStorage.setItem("productos", JSON.stringify(datos));
-
-    txtNombre.value = "";
-    txtNombre.focus();
-    txtAutor.value = "";
-    txtEditorial.value = "";
-    txtISBN.value = "";
-    txtURL.value = "";
-    txtDescripcion.value = "";
-    txtPrecio.value = "";
+        // Crear de nuevo JSON
+        localStorage.setItem("productos", JSON.stringify(datos));
+    
+        txtNombre.value = "";
+        txtNombre.focus();
+        txtAutor.value = "";
+        txtEditorial.value = "";
+        txtISBN.value = "";
+        txtURL.value = "";
+        txtDescripcion.value = "";
+        txtPrecio.value = "";
+     }
+    
 });
--- js/registro_cuenta.js
+++ js/registro_cuenta.js
@@ -3,15 +3,36 @@ let txtNombre = document.getElementById("nombre");
 let txtUsuario = document.getElementById("usuario");
 let txtEmail = document.getElementById("email");
 let txtContrasena = document.getElementById("contrasena");
-
+let txtContrasenaConfirma = document.getElementById("contrasenaConfirma");
 
 botonEnviar.addEventListener("click", (event)=> {
     event.preventDefault();
     const validaciones=[];
-    validaciones.nombre=validarNombre(txtNombre.value);
+     //validaciones.nombre=validarNombre(txtNombre.value);
     validaciones.usuario=validarUsuario(txtUsuario.value);
     validaciones.email=validarEmail(txtEmail.value);
     validaciones.contraseña=validarContraseña(txtContrasena.value);
+    validaciones.contraseniaValidada=validarConfirmaTuContrasena(txtContrasena.value,txtContrasenaConfirma.value);
+    console.log(validaciones);
+
+    if (Object.values(validaciones).every((value) => value === true) ){
+        let datos =  `
+        {"nombre" : "${txtNombre.value}",
+        "usuario" : "${txtUsuario.value}",
+        "email" : "${txtEmail.value}",
+        "contrasenia" : "${txtContrasena.value}"}`;
+        
+        
+        // Crear un nuevo JSON
+        localStorage.setItem("usuario", JSON.stringify(datos));
     
+        txtNombre.value = "";
+        txtNombre.focus();
+
+        txtUsuario.value = "";
+        txtEmail.value = "";
+        txtContrasena.value = "";
+        txtContrasenaConfirma.value ="";
+    }
 
 });
