9bcecc1c7354737365a68457c9cf51f762beb4ae
AbrahamGP11 <abrahamgonzp1@gmail.com>
2022-07-05 08:16:56 -0500

    Se agrega la validación para la creacíon de la cuenta con el correo

--- js/registro_cuenta.js
+++ js/registro_cuenta.js
@@ -12,29 +12,37 @@ botonEnviar.addEventListener("click", (event)=> {
     validaciones.email=validarEmail(txtEmail.value);
     validaciones.contraseña=validarContraseña(txtContrasena.value);
     validaciones.contraseniaValidada=validarConfirmaTuContrasena(txtContrasena.value,txtContrasenaConfirma.value);
-    console.log(validaciones);
-
-    if (Object.values(validaciones).every((value) => value === true) ){
     
-        let elemento =  `
-        {"nombre" : "${txtNombre.value}",
-        "email" : "${txtEmail.value}",
-        "contrasenia" : "${txtContrasena.value}"}`;
+    let usuario=JSON.parse(localStorage.getItem("usuario"));
 
-        datos.push(JSON.parse(elemento))
+    const listaUsuarios=usuario.filter(usuario => usuario.email == txtEmail.value);
+    console.log(listaUsuarios);
+    if (listaUsuarios[0]) {
+        document.getElementById("errorValidacion").style = 'display';
+        document.getElementById("errorValidacion").innerHTML = "Ya existe una cuenta con este correo.<br>";
+    } else{
+        document.getElementById("errorValidacion").style = 'display: none';
+        if (Object.values(validaciones).every((value) => value === true) ){
+    
+            let elemento =  `
+            {"nombre" : "${txtNombre.value}",
+            "email" : "${txtEmail.value}",
+            "contrasenia" : "${txtContrasena.value}"}`;
+    
+            datos.push(JSON.parse(elemento))
+            
+            swal("¡Adelante!", "Cuenta creada exitosamente", "success"); //SweetAlert
+            
+            // Crear un nuevo JSON
+            localStorage.setItem("usuario", JSON.stringify(datos));
         
-        swal("¡Adelante!", "Cuenta creada exitosamente", "success"); //SweetAlert
+            txtNombre.value = "";
+            txtNombre.focus();
+    
         
-        // Crear un nuevo JSON
-        localStorage.setItem("usuario", JSON.stringify(datos));
-    
-        txtNombre.value = "";
-        txtNombre.focus();
-
-    
-        txtEmail.value = "";
-        txtContrasena.value = "";
-        txtContrasenaConfirma.value ="";
+            txtEmail.value = "";
+            txtContrasena.value = "";
+            txtContrasenaConfirma.value ="";
+        }
     }
-
 });
