62c47544afef1c96b1415c5258edcca844e6d951
cristobalgm <cristobalgonzalezm.97@gmail.com>
2022-07-10 00:11:26 -0500

    Correcciones de pagina Inicio, Crear cuenta, Iniciar sesion.

--- js/index.js
+++ js/index.js
@@ -1,28 +1,29 @@
-function productosAleatorios() {
-    const productos = JSON.parse(localStorage.getItem("productos"));
-    for (let index = 0; index < 3; index++) {
-        const numeroAleatorio = Math.floor(Math.random() * (productos.length));
-        addItem(productos[numeroAleatorio]);
+
+
+function productosNovedades() {
+    const productos = JSON.parse(localStorage.getItem("productos")).slice(-4);
+    for (let index = 0; index < 4; index++) {
+        addItem(productos[index]);
     }
 }
 
+
+
 function addItem(item) {
-    const itemHTML =
-        `<div id="contenedorProducto" class="col-md-5 col-lg-3 col-xl-2">
-        <img id="imagenProducto" class="card-img-top p-3" src="${item.img}"/>
-        <div class="card-text">
-            <div class="text-center">
-                <h5>${item.nombre}</h5>
-                <p>$${item.precio}</p>
-            </div>
-        </div>
-           <a id="botonAnadir" href="#">
-            Agregar
-            </a>
-        </div>
-        `;
-    const itemsContainer = document.getElementById("list-items");
-    itemsContainer.innerHTML += itemHTML;
+  const itemHTML =`<div class="col-lg-3 col-md-6 mb-3 justify-content-center)">
+  <div class="card h-100">
+  <div class="img-container d-flex justify-content-center h-75 p-2">
+  <img src="${item.img}" alt="" class="image card-img-top" style="width: min(300px, 70%); height: min(25rem, 100%);">  
+  </div>
+  <div class="card-body h-25 d-flex flex-column justify-content-center align-items-center mt-3 mb-2" style="min-height: fit-content;">
+  <h5 class="card-title text-center">${item.nombre}</h5>
+  <h6 class="card-subtitle text-muted text-center">$${item.precio}</h6>
+  </div>
+  </div>
+  </div>`
+  const itemsContainer = document.getElementById("list-items");
+  itemsContainer.innerHTML += itemHTML;
 }
 
-productosAleatorios();
+productosNovedades();
+
